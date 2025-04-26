e0a278fcb7eb46d4e13a88e480285a57b72a2ace
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2025-04-26 15:49:29 +0200

    Added regex validation for date in WorkTimeInputDto.php

--- src/Dto/WorkTimeInputDto.php
+++ src/Dto/WorkTimeInputDto.php
@@ -7,6 +7,8 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
 
 class WorkTimeInputDto  {
     public const DATE_FORMAT = 'd.m.Y H:i';
+        private const DATE_REGEX = '/^\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}$/';
+
 
     #[Assert\NotBlank(message: "Identyfikator nie może być pusty.")]
     #[Assert\Uuid(message: "Identyfikator musi być poprawnym UUID.")]
@@ -14,10 +16,17 @@ class WorkTimeInputDto  {
     public ?string $employeeId = null;
 
     #[Assert\NotBlank(message: "Początek czasu pracy nie może być pusty.")]
+    #[Assert\Regex(
+        pattern: self::DATE_REGEX,
+        message: "Początek czasu pracy musi być w formacie 'dd.mm.yyyy hh:mm'.")]
     #[SerializedName('data_i_godzina_rozpoczecia')]
     public ?string $startTimeString = null;
 
     #[Assert\NotBlank(message: "Koniec czasu pracy nie może być pusty.")]
+    #[Assert\Regex(
+        pattern: self::DATE_REGEX,
+        message: "Koniec czasu pracy musi być w formacie 'dd.mm.yyyy hh:mm'."
+    )]
     #[SerializedName('data_i_godzina_zakonczenia')]
     public ?string $endTimeString = null;
 }
