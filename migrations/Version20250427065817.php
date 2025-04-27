5964d5dc84209aaac26b6290cc01e82ad28a5cee
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2025-04-27 09:41:13 +0200

    Create migration for Error Reporting

---
+++ migrations/Version20250427065817.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250427065817 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE error_report (id BINARY(16) NOT NULL COMMENT '(DC2Type:uuid)', error_class VARCHAR(255) NOT NULL, error_message LONGTEXT NOT NULL, error_code INT DEFAULT NULL, trace LONGTEXT NOT NULL, context JSON DEFAULT NULL COMMENT '(DC2Type:json)', request_uri VARCHAR(512) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE error_report
+        SQL);
+    }
+}
